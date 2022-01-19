d22d8283f15e3f2c4b4dee21a97adf28358485b0
Sumit <sumitgadae011@gmail.com>
2022-01-19 10:59:54 +0530

    Search Functionality first commit

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -10,7 +10,11 @@ import "../App.css";
 const { Title } = Typography;
 
 function TableComponent() {
+  const [originalData, setOriginalData] = useState([]);
   const [data, setData] = useState([]);
+  const [timer, setTimer] = useState(null);
+  const [showTable, setShowTable] = useState(true);
+  const [notFound, setNotfound] = useState(false);
 
   useEffect(() => {
     getData();
@@ -19,12 +23,41 @@ function TableComponent() {
   const getData = () => {
     DataService.getData()
       .then((response) => {
+        setOriginalData(response.data.results);
         setData(response.data.results);
         console.log(response.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  const handleSearch = (e) => {
+    clearTimeout(timer);
+    const newTimer = setTimeout(() => {
+      searchFunction(e.target.value);
+    }, 2000);
+    setTimer(newTimer);
+  };
+
+  const searchFunction = (val) => {
+    if (val !== "") {
+      console.log(val);
+      const filterData = originalData.filter((entry) =>
+        entry.buyer_name.toLowerCase().includes(val.toLowerCase())
+      );
+      if (!filterData.length) {
+        setShowTable(false);
+        setNotfound(true);
+        setData([]);
+      } else {
+        setData(filterData);
+      }
+    } else {
+      setShowTable(true);
+      setNotfound(false);
+      setData(originalData);
+    }
   };
 
   data.map((d) => {
@@ -83,21 +116,27 @@ function TableComponent() {
               <Input
                 placeholder="input serch text"
                 style={{ width: 250, margin: 20 }}
+                onChange={handleSearch}
               />
             </Title>
           </div>
-          <Table
-            title={() => "Data Display"}
-            columns={columns}
-            bordered
-            dataSource={data}
-            pagination={false}
-            onRow={(r) => ({
-              onClick: () => {
-                window.open(`/data/view/${r.id}`, "_parent");
-              },
-            })}
-          />
+          <div>
+            {showTable ? (
+              <Table
+                title={() => "Data Display"}
+                columns={columns}
+                bordered
+                dataSource={data}
+                pagination={false}
+                onRow={(r) => ({
+                  onClick: () => {
+                    window.open(`/data/view/${r.id}`, "_parent");
+                  },
+                })}
+              />
+            ) : null}
+          </div>
+          {notFound ? <div className="notFound">Record Not Found</div> : null}
         </div>
       </Content>
       <Footer style={{ textAlign: "center" }}>
